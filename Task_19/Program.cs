52ad425296a14a576abbb1700939dd74f25b8220
Popova Ekaterina <popova_katerina97@mail.ru>
2022-08-15 01:00:05 +0500

    Task 19

---
+++ Task_19/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 19
+//Напишите программу, 
+//которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string strnumber = Convert.ToString(number);
+if (strnumber[0]==strnumber[4] || strnumber[1]==strnumber[3])
+{
+    Console.WriteLine("Число является палиндромом!!!");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
+//14212 -> нет
+//12821 -> да
+//23432 -> да
